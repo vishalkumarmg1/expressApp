1bed2fc3d286e0921053ff5e20c56ae013bfc061
Vishal <vishal.mgvk@gmail.com>
2020-04-24 16:54:30 -0400

    Added the a function that logs the data(middleware), using postman tested the logger function

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: 'Vishal',
+    email: 'xyz@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Bob',
+    email: 'abc@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Shannon',
+    email: 'pqr@gmail.com',
+    status: 'active'
+  }
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const moment = require('moment')
+const members = require('./Members')
+
 
 const app = express();
 
@@ -8,8 +11,15 @@ const app = express();
 
 // });
 
+const logger = (req,res, next)=>{
+    console.log(`${req.protocal}://${req.get('host')}${req.originalUrl}: ${moment()}`);
+    next();
+};  
+app.use(logger)
+
+app.get('/api/members',(req, res)=>res.json(members));
 app.use(express.static(path.join(__dirname,'public')));
-const PORT =process.env.PORT || 5202;
+const PORT =process.env.PORT || 5222;
 
 
 app.listen(PORT, ()=>{
